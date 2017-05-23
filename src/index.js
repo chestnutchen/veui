760b6f81eef734a5cf92d009fba0a1f9d5128c40
chestnut <mini.chenli@gmail.com>
2017-05-24 00:20:50 +0800

    添加Form和validator

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import { setBaseZIndex, addOverlay } from './managers/overlay'
+import { addRule } from './utils/validators'
 
 export default {
   install (Vue, { baseZIndex = 100 } = {}) {
     setBaseZIndex(baseZIndex)
     Vue.prototype.$veui = {
-      addOverlay
+      addOverlay,
+      Validator: {
+        addRule
+      }
     }
   }
 }
