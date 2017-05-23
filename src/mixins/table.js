00c7d99f645411783d1b963e85b53fa54e3ef7df
chestnut <mini.chenli@gmail.com>
2017-05-24 00:20:50 +0800

    添加Form和validator

--- src/mixins/table.js
+++ src/mixins/table.js
@@ -1,17 +1,2 @@
-import { includes } from 'lodash'
-
-export default {
-  computed: {
-    table () {
-      let current = this.$parent
-      while (current) {
-        let { uiTypes } = current.$options
-        if (uiTypes && includes(uiTypes, 'table')) {
-          return current
-        }
-        current = current.$parent
-      }
-      return null
-    }
-  }
-}
+import { genParentTracker } from '../utils/helper'
+export default genParentTracker('table')
