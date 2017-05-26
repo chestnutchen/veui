b5e087cd90f366671bc46e1e2e33e6e2c9aabab9
chestnut <mini.chenli@gmail.com>
2017-05-26 14:38:02 +0800

    添加动态表单，修改validator配置结构

--- src/components/Checkbox.vue
+++ src/components/Checkbox.vue
@@ -24,9 +24,7 @@ export default {
   mixins: [input],
   props: {
     ui: String,
-    name: String,
-    value: String,
-    disabled: Boolean,
+    value: null,
     checked: Boolean,
     indeterminate: Boolean
   },
